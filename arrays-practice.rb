8f9a380e97f4adee3ebbe5dd416801e7c6350d35
Shinan Liao <shinanliao1@gmail.com>
2021-05-18 14:55:08 -0700

    more array practice solutions with while loops and each method

--- arrays-practice.rb
+++ arrays-practice.rb
@@ -198,3 +198,199 @@ numbers.each do|number|
     odd_numbers << number
   end
 end
+
+
+#  11. Start with an array of numbers and compute the sum of all the numbers.
+numbers = [5, 10, 8, 3]
+
+sum = 0
+index = 0
+while index < numbers.length
+  sum = numbers[index] + sum
+  index += 1
+end
+
+numbers = [5, 10, 8, 3]
+sum = 0
+numbers.each do |number|
+  sum = number + sum
+end
+
+
+#  12. Start with an array of strings and combine them all into a single string.
+string = ["volleyball", "basketball", "badminton"]
+word = ""
+
+index = 0
+while index < string.length
+  word << string[index]
+  index += 1
+end
+
+string = ["volleyball", "basketball", "badminton"]
+words = ""
+string.each do|word|
+  words << word
+end
+
+
+#  13. Start with an array of hashes and compute the sum of the prices (from the :price key).
+products = [
+  {name: "chair", price: 100}, 
+  {name: "pencil", price: 1}, 
+  {name: "book", price: 4}
+]
+sum_of_products = []
+
+index = 0
+price = 0
+while index < products.length
+  sum = products[index][:price]
+  price = sum + price
+  index += 1
+end
+
+price = 0
+products.each do |product|
+  price += product[:price]
+end
+
+
+#  14. Start with an array of numbers and compute the the minumum number.
+numbers = [5, 10, 8, 3, 9]
+index = 0
+base_number = numbers[0]
+while index < numbers.length
+  number = numbers[index]
+  if number < base_number
+    base_number = number
+  end
+  index += 1
+end
+
+base_number = numbers[0]
+numbers.each do |number|
+  if number < base_number
+    base_number = number
+  end
+end
+
+
+#  15. Start with an array of strings and compute the total length of all the strings.
+
+strings = ["volleyball", "basketball", "badminton"]
+
+sum = 0
+index = 0
+while index < strings.length
+  sum = strings[index].length + sum
+  index += 1
+end
+
+sum = 0
+strings.each do|string|
+  sum = string.length + sum
+end
+
+
+#  16. Start with an array of hashes and find the hash with the lowest price (from the :price key).
+
+products = [
+  {name: "chair", price: 100}, 
+  {name: "pencil", price: 1}, 
+  {name: "book", price: 4}
+]
+cheapest = products[0]
+index = 0
+while index < products.length
+  product = products[index]
+  if product[:price] < cheapest[:price]
+    cheapest = product
+  end
+  index += 1
+end
+
+cheapest = products[0]
+products.each do|product|
+  if product[:price] < cheapest[:price]
+    cheapest = product
+  end
+end
+
+
+#  17. Start with an array of numbers and compute product of all the numbers.
+
+numbers = [5, 10, 8, 3]
+product = 1
+index = 0
+while index < numbers.length
+  product = numbers[index] * product
+  index += 1
+end
+
+product = 1
+numbers.each do|number|
+  product = number * product
+end
+
+
+#  18. Start with an array of strings and combine them all into a single string, separated by dashes.
+
+array = ["volleyball", "basketball", "badminton"]
+strings = ""
+index = 0
+while index < array.length
+  strings << array[index] + "-"
+  index += 1
+end]
+
+strings = ""
+array.each do |word|
+  strings << word + "-"
+end
+
+
+#  19. Start with an array of hashes and find the hash with the shortest name (from the :name key).
+
+products = [
+  {name: "chair", price: 100}, 
+  {name: "pencil", price: 1}, 
+  {name: "book", price: 4}
+]
+
+shortest = products[0]
+index = 0
+while index < products.length
+  product = products[index]
+  if product[:name].length < shortest[:name].length
+    shortest = product
+  end
+  index += 1
+end
+
+shortest = products[0]
+products.each do |product|
+  if product[:name].length < shortest[:name].length
+    shortest = product
+  end
+end
+
+
+# 20. Start with an array of numbers and compute the maximum number.
+numbers = [5, 10, 8, 3]
+
+maximum = numbers[0]
+index = 0
+while index < numbers.length
+  if numbers[index] > maximum
+    maximum = numbers[index]
+  end
+  index += 1
+end
+
+maximum = numbers[0]
+numbers.each do |number|
+  if number > maximum
+    maximum = number
+  end
+end
