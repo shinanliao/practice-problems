498a64e8de5701817adaca4d9f80f6e9d84812d4
Shinan Liao <shinanliao1@gmail.com>
2021-07-20 16:18:39 -0700

    adds practice problems on nested loops

--- nested-loops-practice.rb
+++ nested-loops-practice.rb
@@ -26,3 +26,39 @@ while first_index < abc_strings.length
   end
   first_index += 1
 end
+
+#  3. Use a nested loop with one array of strings to create a new array that contains every combination of each string with every other string in the array.
+abc_strings = ["a", "b", "c", "d"]
+new_string = []
+index = 0
+while index < abc_strings.length
+  second_index = 0
+  while second_index < abc_strings.length
+    if index != second_index
+      new_string << abc_strings[index] + abc_strings[second_index]
+    end
+    second_index += 1
+  end
+  index += 1
+end
+
+#  4. Use a nested loop to find the largest product of any two different numbers within a given array.
+numbers = [5, -2, 1, -9, -7, 2, 6]
+largest_product = numbers[0] * numbers[1]
+index = 0
+while index < numbers.length
+  number = numbers[index]
+  second_index = 0
+  while second_index < numbers.length
+    if index != second_index
+      number2 = numbers[second_index]
+      product = number * number2
+      if product > largest_product
+        largest_product = product
+      end
+    end
+    second_index = second_index + 1
+  end
+  index = index + 1
+end
+p largest_product
--- whiteboarding.rb
+++ whiteboarding.rb
@@ -34,10 +34,57 @@
 
 
 
-1. Write a function that takes in an array of numbers and returns a new array of the same numbers without any duplicates.
+# Write a function that takes in an array of numbers and returns a new array of the same numbers without any duplicates.
 
 #define a function that takes in an array of numbers and prints it
 #writes a function that loops through the array of numbers and prints out each number
 
 #expected input = [1, 1, 3, 20, 120]
 #expected output = [1, 3, 20, 120]
+
+
+# Write a function that takes in two arrays of numbers and returns all the numbers that appear in both arrays.
+# expected input = 
+# first_array = [9, 10, 31, 12, 99]
+# second_array = [10, 102, 12, 29, 14]
+
+# expected output =
+# common_numbers = [10, 12]
+
+# clarify
+
+# visualize
+# create a new array
+# loop through the first array
+
+# Write a function that takes in a sorted array of words and uses binary search to find the index of the word “hello” in the array.
+
+# expected input = 
+# words = ["apple", "banana", "chicken", "dinner", "hello", "goodbye", "zebra"]
+
+# expected output = 
+# index = 4
+
+# visualize
+# 1. print the array of words
+# 2. write a function that loops through the input array and prints out each word
+# 3. write a function that loops through the input array with an if condition that prints true when it matches the word hello
+
+# def print_each_word(words)
+#   words.each {|word| puts "true" if word == "hello"}
+# end
+
+# print_each_word(["apple", "banana", "chicken", "dinner", "hello", "goodbye", "zebra"])
+
+# Write a function that takes in an array of numbers and returns the 3rd largest number.
+# expected input = 
+# numbers = [10, 1, 12, 9, 15, -19]
+
+# expected output =
+# number = 10
+
+# visualize
+# 1. Print numbers array
+# 2. Set a variable to the first index of the array
+# 3. Write a function that loops through the input array
+# 4. Write a function that loops through the input array and compare each number to the variable of the first index
